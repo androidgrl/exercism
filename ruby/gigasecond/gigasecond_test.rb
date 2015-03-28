ad15cb309d95d7dece7f437b61e90ad1a616af6d
androidgrl <kawaharajamie@gmail.com>
2015-03-28 13:49:57 -0600

    adding gigasecond exercism

---
+++ ruby/gigasecond/gigasecond_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'date'
+require 'time'
+
+require_relative 'gigasecond'
+class GigasecondTest < Minitest::Test
+  def test_1
+    gs = Gigasecond.from(Time.utc(2011, 4, 25))
+    assert_equal Time.utc(2043, 1, 1, 1, 46, 40), gs
+  end
+
+  def test_2
+    gs = Gigasecond.from(Time.utc(1977, 6, 13))
+    assert_equal Time.utc(2009, 2, 19, 1, 46, 40), gs
+  end
+
+  def test_3
+    gs = Gigasecond.from(Time.utc(1959, 7, 19))
+    assert_equal Time.utc(1991, 3, 27, 1, 46, 40), gs
+  end
+
+  def test_4_with_seconds
+    gs = Gigasecond.from(Time.utc(1959, 7, 19, 23, 59, 59))
+    assert_equal Time.utc(1991, 3, 28, 1, 46, 39), gs
+  end
+
+  def test_5_with_your_birthday
+    your_birthday = Time.utc(1977, 2, 27)
+    gs = Gigasecond.from(your_birthday)
+    assert_equal Time.utc(2008, 11, 5, 1, 46, 40), gs
+  end
+end
