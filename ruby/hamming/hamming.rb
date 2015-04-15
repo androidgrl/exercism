478586f1863b522765efad9549aea7ae1aa4030e
androidgrl <kawaharajamie@gmail.com>
2015-04-15 14:06:22 -0600

    adding hamming test

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,12 @@
+require 'byebug'
+
+class Hamming
+  def self.compute(strand1, strand2)
+    array1 = strand1.chars
+    array2 = strand2.chars
+    together = array1.zip(array2)
+    together.count do |n1, n2|
+      n1 != n2
+    end
+  end
+end
