4e4a52795562acfe689830f00ef6d5e513a55c92
androidgrl <kawaharajamie@gmail.com>
2015-04-16 12:34:30 -0600

    raindrops challenge

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,18 @@
+class Raindrops
+  def self.convert(num)
+    answer = []
+      if num % 3 == 0
+        answer << "Pling"
+      end
+      if num % 5 == 0
+        answer << "Plang"
+      end
+      if num % 7 == 0
+        answer << "Plong"
+      end
+      if num % 3 != 0 && num % 5 != 0 && num % 7 != 0
+        answer << num.to_s
+      end
+    answer.join
+  end
+end
