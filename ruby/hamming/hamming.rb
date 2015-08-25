f852685a008048a8b910781ffb04d5841a4dc8e5
androidgrl <kawaharajamie@gmail.com>
2015-08-25 15:46:23 -0600

    refactor hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,7 @@
-require 'byebug'
-
 class Hamming
   def self.compute(strand1, strand2)
-    array1 = strand1.chars
-    array2 = strand2.chars
-    together = array1.zip(array2)
-    together.count do |n1, n2|
-      n1 != n2
-    end
+    pairs = strand1.chars.zip(strand2.chars)
+    pairs.count { |x,y| x!=y }
   end
-
+end
 
