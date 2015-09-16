d1ee70cc381d16afc864ad7b6c7cfb9d759fcdcd
androidgrl <kawaharajamie@gmail.com>
2015-09-16 16:12:44 -0600

    Add bob and hello world js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,16 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    if (input.trim().length === 0) {
+       return 'Fine. Be that way!'
+    } else if ((input.match(/[a-z]/i)) && (input.toUpperCase() === input)) {
+        return 'Whoa, chill out!'
+    } else if (input.slice(-1) === "?") {
+        return 'Sure.'
+    } else {
+        return 'Whatever.'
+    }
 };
 
 module.exports = Bob;
