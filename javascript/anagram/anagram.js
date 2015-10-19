68dbe76154c63e673472f0fbb688f7d8de4ad999
androidgrl <kawaharajamie@gmail.com>
2015-10-18 22:44:20 -0600

    Add anagram

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,29 @@
+var anagram = function (word) {
+    return {
+        matches: matches.bind(this),
+    };
+
+    function matches() {
+        var array = [];
+        for (var i = 0; i < arguments.length; i++) {
+            array.push(arguments[i]);
+        }
+        var flattenedArray = [].concat.apply([], array);
+        var nonDuplicateArray = flattenedArray.filter(function(element){
+            return element.toLowerCase() !== word.toLowerCase();
+        });
+        var result = nonDuplicateArray.filter(function(element){
+            return element.toLowerCase().split('').sort().join() ===  word.toLowerCase().split('').sort().join();
+        });
+        return result;
+    }
+};
+module.exports = anagram;
+
+
+//function addingSomething(argument) {
+//return "added" + argument;
+//}
+
+//addingSomething("hello")
+//addingSomething.call(null, "hello");
